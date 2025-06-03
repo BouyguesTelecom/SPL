ffce4dc2d9f3c631c9598dcde0bf4ae461232381
Arnaud Leymet <aleymet@bouyguestelecom.fr>
2025-06-03 15:41:24 +0200

    :tada: First release

---
+++ src/index.ts
@@ -0,0 +1,67 @@
+import { defaultFieldResolver, GraphQLSchema } from 'graphql'
+import { MeshTransform } from '@graphql-mesh/types'
+import { MapperKind, mapSchema } from '@graphql-tools/utils'
+import { splListFilterer } from './main'
+
+export default class SplDirectiveTransform implements MeshTransform {
+  noWrap = true
+
+  transformSchema(schema: GraphQLSchema) {
+    return mapSchema(schema, {
+      [MapperKind.OBJECT_FIELD]: (fieldConfig) => {
+        const originalResolver = fieldConfig.resolve ?? defaultFieldResolver
+
+        const resolver = async (next: any , _source: any, _args: any, context: any, info: any) => {
+          const { directives } = info.fieldNodes[0]
+          const splDirective = directives.find((directive: { name: { value: string } }) => directive.name.value === 'SPL')
+          const result = await next(context)
+
+          if (splDirective) {
+            const { value } = splDirective.arguments[0]
+
+            const data = splListFilterer.filter(
+              value.value,
+              splListFilterer.formatInput(result),
+              splListFilterer.formatVariables(context.params.variables || {})
+            )
+            return splListFilterer.formatOutput(data)
+          }
+
+          return result
+        }
+
+        fieldConfig.resolve = (source, originalArgs, context, info) => {
+          return resolver(
+            (context: unknown) =>
+              new Promise((resolve, reject) => {
+                const result = originalResolver(source, originalArgs, context, info)
+                if (result instanceof Error) {
+                  reject(result)
+                }
+                resolve(result)
+              }),
+            source,
+            originalArgs,
+            context,
+            info
+          )
+        }
+        return fieldConfig
+      }
+    })
+  }
+}
+
+export const splDirectiveTypeDef: string = /* GraphQL */ `
+  """
+  This is a very small, lightweight, straightforward and non-evaluated expression language to sort, filter and paginate arrays of maps.
+  """
+  directive @SPL(
+    """
+    SPL query
+
+    > [Usage examples](https://gitlab.int.nbyt.fr/graphql/spl/-/blob/master/README.md#examples)
+    """
+    query: String
+  ) on FIELD
+`
---
+++ src/main/sorter/SPLListFilterer.ts
@@ -0,0 +1,94 @@
+import { SPLPredicateFilter } from '../predicate/SPLPredicateFilter'
+import { SPLLimitExtractor } from '../limiter/SPLLimitExtractor'
+import { SPLQueryToTreeConverter } from '../antlr/SPLQueryToTreeConverter'
+import { SPLComparatorFactory } from './SPLComparatorFactory'
+import { QueryContext } from '../antlr/SPLParser'
+import { Limit } from '../limiter/Limit'
+
+export class SPLListFilterer {
+  private splPredicateFilter: SPLPredicateFilter
+  private splLimitExtractor: SPLLimitExtractor
+  private splComparatorFactory: SPLComparatorFactory
+  private splQueryToTreeConverter: SPLQueryToTreeConverter
+
+  constructor(
+    splPredicateFilter: SPLPredicateFilter,
+    splLimitExtractor: SPLLimitExtractor,
+    splComparatorFactory: SPLComparatorFactory,
+    splQueryToTreeConverter: SPLQueryToTreeConverter,
+  ) {
+    this.splPredicateFilter = splPredicateFilter
+    this.splLimitExtractor = splLimitExtractor
+    this.splComparatorFactory = splComparatorFactory
+    this.splQueryToTreeConverter = splQueryToTreeConverter
+  }
+
+  public filter(
+    query: string,
+    input: Map<string, Object>[],
+    variables: Map<string, Object> = new Map(),
+  ): Map<string, Object>[] {
+    const queryContext: QueryContext = this.splQueryToTreeConverter.createTree(query)
+    const limitObj: Limit = this.splLimitExtractor.fetchLimit(queryContext, variables)
+
+    return input
+      .filter((stringObjMap) =>
+        this.splPredicateFilter.filter(queryContext, stringObjMap, variables),
+      )
+      .slice(limitObj.offset, limitObj.offset + limitObj.limit)
+      .sort(this.splComparatorFactory.createComparator(queryContext))
+  }
+
+  public formatInput(input: any): Map<string, Object>[] {
+    const parsedJsonInput = JSON.parse(JSON.stringify(input))
+    const inputMap: Map<string, Object>[] = []
+
+    parsedJsonInput.forEach((item: Map<string, Object>) => {
+      const newItem: Map<string, Object> = new Map()
+
+      Object.entries(item).forEach(([key, value]) => {
+        if (value && typeof value === 'object') {
+          newItem.set(key, new Map(Object.entries(value)))
+        } else {
+          newItem.set(key, value)
+        }
+      })
+
+      inputMap.push(newItem)
+    })
+    return inputMap
+  }
+
+  public formatVariables(variables: any): Map<string, Object> {
+    const newItem: Map<string, Object> = new Map()
+    if (!Array.isArray(variables)) {
+      Object.entries(variables).forEach(([key, value]) => {
+        if (value && typeof value === 'object') {
+          newItem.set(key, new Map(Object.entries(value)))
+        } else {
+          newItem.set(key, value as Object)
+        }
+      })
+    }
+    return newItem
+  }
+
+  public formatOutput(output: Map<string, Object>[]): any[] {
+    const reversedInput: any[] = []
+
+    output.forEach((item: Map<string, Object>) => {
+      const newItem: any = {}
+
+      item.forEach((value, key) => {
+        if (value instanceof Map) {
+          newItem[key] = Object.fromEntries(value)
+        } else {
+          newItem[key] = value
+        }
+      })
+
+      reversedInput.push(newItem)
+    })
+    return reversedInput
+  }
+}
